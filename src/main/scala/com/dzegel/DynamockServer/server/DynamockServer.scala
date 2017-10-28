7b0b4005d03965c6adcc8f0cb7d493efa5a8a22c
dzegel <dzegel@gmail.com>
2017-10-29 01:41:13 +0200

    port configuration

--- src/main/scala/com/dzegel/DynamockServer/server/DynamockServer.scala
+++ src/main/scala/com/dzegel/DynamockServer/server/DynamockServer.scala
@@ -5,12 +5,29 @@ import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.routing.HttpRouter
 
 class DynamockServer extends HttpServer {
-  override val defaultFinatraHttpPort: String = ":8080"
-  override val disableAdminHttpServer = true
+  override protected lazy val defaultFinatraHttpPort: String = DynamockServer.tryGetPortFromArgs(args)
+
+  override protected val disableAdminHttpServer = true
+
+  override protected val allowUndefinedFlags: Boolean = true
 
   override protected def configureHttp(router: HttpRouter): Unit = {
     router
       .add[ExpectationController]
       .add[MockController]
+  }
+}
+
+object DynamockServer {
+  private def tryGetPortFromArgs(args: Array[String]): String = args.collect {
+    case arg if arg.startsWith("port=") =>
+      if (arg.matches("""port=([\d]{4})"""))
+        arg.substring(5)
+      else
+        throw new Exception("Dynamock Initialization Error: Argument 'port' must be a 4 digit value.")
+  } match {
+    case Array() => ":8080"
+    case Array(port) => ":" + port
+    case _ => throw new Exception("Dynamock Initialization Error: At most one 'port' argument can be defined.")
   }
 }
