aee587601b164e9f9c63b25f2651fc877d28567f
dzegel <dzegel@gmail.com>
2018-01-20 20:28:54 +0200

    tests

--- src/test/scala/com/dzegel/DynamockServer/registry/RegistryValuesInjectionModuleTests.scala
+++ src/test/scala/com/dzegel/DynamockServer/registry/RegistryValuesInjectionModuleTests.scala
@@ -6,14 +6,30 @@ import org.scalatest.{FunSuite, Matchers}
 
 class RegistryValuesInjectionModuleTests extends FunSuite with Matchers {
 
-  test("""fileRootRegistry(":80") works""") {
-    RegistryValuesInjectionModule.fileRootRegistry(":80").fileRoot shouldBe
-      s"${File.listRoots.head.getCanonicalPath}${File.separator}Dynamock${File.separator}80"
+  test("""fileRootRegistry(":65534") works""") {
+    RegistryValuesInjectionModule.fileRootRegistry(":65534").fileRoot shouldBe
+      s"${File.listRoots.head.getCanonicalPath}${File.separator}Dynamock${File.separator}65534"
   }
 
-  test("""fileRootRegistry(":0080") strips leading 0s""") {
-    RegistryValuesInjectionModule.fileRootRegistry(":0080").fileRoot shouldBe
-      s"${File.listRoots.head.getCanonicalPath}${File.separator}Dynamock${File.separator}80"
+  test("""fileRootRegistry(":002") strips leading 0s""") {
+    RegistryValuesInjectionModule.fileRootRegistry(":002").fileRoot shouldBe
+      s"${File.listRoots.head.getCanonicalPath}${File.separator}Dynamock${File.separator}2"
+  }
+
+  test("""fileRootRegistry(":1") throws""") {
+    val thrown = intercept[Exception] {
+      RegistryValuesInjectionModule.fileRootRegistry(":1")
+    }
+    thrown.getMessage shouldBe
+      "Dynamock Initialization Error: 'http.port' flag must be a colon prefixed integer in the range [2, 65534] (i.e. :8080)."
+  }
+
+  test("""fileRootRegistry(":65535") throws""") {
+    val thrown = intercept[Exception] {
+      RegistryValuesInjectionModule.fileRootRegistry(":65535")
+    }
+    thrown.getMessage shouldBe
+      "Dynamock Initialization Error: 'http.port' flag must be a colon prefixed integer in the range [2, 65534] (i.e. :8080)."
   }
 
   test("""expectationsUrlPathBaseRegistry("test") adds leading /""") {
